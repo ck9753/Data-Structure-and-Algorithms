8defac12e03d14dfe39a48265825367fef869bf9
yk234 <yk234@duke.edu>
2022-09-16 13:22:40 -0400

    Update 28

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -5,14 +5,17 @@ unsigned power(unsigned x, unsigned y);
 
 void run_check(unsigned x, unsigned y, unsigned expected_ans) {
   if (power(x, y) != expected_ans) {
-    return exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 }
-int main(unsigned x, unsigned y) {
+int main(void) {
   run_check(2, 3, 8);
   run_check(0, 0, 1);
   run_check(8, 3, 512);
   run_check(9, 0, 1);
   run_check(16, 5, 1048576);
   run_check(5, 5, 3125);
+
+  printf("check all cases and no errors");
+  return EXIT_SUCCESS;
 }
