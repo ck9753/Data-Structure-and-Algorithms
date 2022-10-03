a7e320c767f0b68b48f395c4fa10fb7b95ad9037
yk234 <yk234@duke.edu>
2022-10-03 17:04:13 -0400

    Did Assignment 49

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -1,14 +1,50 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void rot_matrix(FILE * f) {
   char rot_matrix[10][10] = {{0}};
   char x[12] = {0};
   for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      rot_matrix[j][9 - i] = x[j];
+    if ((fgets(x, 12, f) != NULL)) {
+      if (strchr(x, '\n') == NULL) {
+        perror("more than 10 characters\n");
+        exit(EXIT_FAILURE);
+      }
+
+      if (x[10] != '\n') {
+        perror("incorrect number of char\n");
+        exit(EXIT_FAILURE);
+      }
+
+      if (x[0] == '\n') {
+        perror("less than 10 rows\n");
+        exit(EXIT_FAILURE);
+      }
+
+      else {
+        for (int j = 0; j < 10; j++) {
+          if (x[j] == '\n') {
+            perror("incorrect number of char\n");
+            exit(EXIT_FAILURE);
+          }
+
+          else {
+            rot_matrix[j][9 - i] = x[j];
+          }
+        }
+      }
     }
+    else {
+      perror("less than 10 rows\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (fgets(x, 12, f) != NULL) {
+    perror("more than 10 rows\n");
+    exit(EXIT_FAILURE);
   }
   for (int y = 0; y < 10; y++) {
     for (int z = 0; z < 10; z++) {
