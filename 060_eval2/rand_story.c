2943c471e03a024db61d4eda58f6e0bdb47f1dd1
yk234 <yk234@duke.edu>
2022-10-23 17:55:50 -0400

    Did Assignment 70

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -1,1 +1,29 @@
 #include "rand_story.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const char * chooseWord(char * category, catarray_t * cats) {
+  // if the category dose not have any words, the function will exit the program with a failure status
+  if (category == NULL) {
+    fprintf(stderr, "category does not have any words");
+    exit(EXIT_FAILURE);
+  }
+
+  char * cat = "cat";
+
+  if (cats == NULL) {
+    return cat;
+  }
+
+  else {
+    for (size_t i = 0; i < cats->n; i++) {
+      if (cats->arr[i].name == category) {
+        // check if it's right string to point
+        return cats->arr[i].name;
+      }
+    }
+    fprintf(stderr, "The requested category does not exist in cats");
+    exit(EXIT_FAILURE);
+  }
+}
