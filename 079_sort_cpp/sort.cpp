975d8c8bd539dbd2191d0dca70068738e60e362d
yk234 <yk234@duke.edu>
2022-10-29 17:58:00 -0400

    Did Assignment 79

---
+++ 079_sort_cpp/sort.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void sort_a_file(std::istream & x) {
+  std::string s;
+  std::vector<std::string> line;
+
+  while (getline(x, s)) {
+    line.push_back(s);
+  }
+
+  if ((x.eof() != 1) && (x.good() != 1)) {
+    std::cerr << "Error for reading lines" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::sort(line.begin(), line.end());
+
+  std::vector<std::string>::iterator iter = line.begin();
+
+  while (iter != line.end()) {
+    std::cout << *iter << std::endl;
+    iter++;
+  }
+}
+
+void sort_files(int argc, char ** argv) {
+  for (int i = 1; i < argc; i++) {
+    std::ifstream y(argv[i], std::ifstream::in);
+
+    if (!y.is_open()) {
+      std::cerr << "Failed to open File" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    sort_a_file(y);
+    y.close();
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc > 1) {
+    sort_files(argc, argv);
+  }
+
+  else if (argc == 1) {
+    sort_a_file(std::cin);
+  }
+
+  return EXIT_SUCCESS;
+}
