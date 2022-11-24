4af5fe5a0e1524f66c055e7a2272fb6d8dbc6944
yk234 <yk234@duke.edu>
2022-11-24 14:39:48 -0500

    Did Assignment 104

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,14 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
-}
+  if (sym != NO_SYM) {
+    theMap.insert(std::pair<unsigned, BitString>(sym, b));
+  }
 
+  else {
+    BitString bit_left = b.plusZero();
+    this->left->buildMap(bit_left, theMap);
+    BitString bit_right = b.plusOne();
+    this->right->buildMap(bit_right, theMap);
+  }
+}
